092e0955a78763cc45bd120ff43ced6c76af7d7e
sk4rdz <3st021c@gmail.com>
2021-06-06 13:06:30 +0900

    グラフの実装

--- solve.cpp
+++ solve.cpp
@@ -53,14 +53,74 @@ template<typename T> inline bool chmax(T &a, T b) { if (a < b) { a = b; return 1
 template<typename T> inline bool chmin(T &a, T b) { if (a > b) { a = b; return 1; } return 0; }
 #pragma endregion
 
+struct Graph {
+    int h, w, n;
+    vector<vector<pi>> g;
+    Graph(int h, int w) : h(h), w(w), n(h*w) {
+        g = vector<vector<pi>>(n);
+    }
+    void add(int sy, int sx, int ty, int tx) {
+        g[sy*w+sx].push_back({ty, tx});
+    }
+    vector<pi> &get(int y, int x) {
+        return g[y*w+x];
+    }
+};
+
+map<char, pi> DIR =
+    {{'U', {-1, 0}}, {'D', {1, 0}},
+     {'L', {0, -1}}, {'R', {0, 1}}};
+
+int N, M;
+vvi board;
+vector<string> S;
+
+pi check_order(int y, int x, string &order) {
+    if (board[y][x]) return {-1, -1};
+    for (char d: order) {
+        auto [dy, dx] = DIR[d];
+        y += dy, x += dx;
+        if (y < 0 or x < 0 or y >= N or x >= N) return {-1, -1};
+        if (board[y][x]) return {-1, -1};
+    }
+    return {y, x};
+}
+
 void solve() {
+    Graph graph = Graph(N, N);
     
+    rep(i, N) rep(j, N) {
+        for (string &order: S) {
+            auto [ni, nj] = check_order(i, j, order);
+            if (ni != -1) graph.add(i, j, ni, nj);
+        }
+    }
+    /*
+    rep(i, N) rep(j, N) {
+        print(i, j);
+        print(graph.get(i, j));
+        print("---");
+    }
+    */
+}
+
+void input() {
+    cin >> N;
+    board = vvi(N, vi(N));
+    rep(i, N) {
+        int y, x;
+        cin >> y >> x;
+        board[y-1][x-1] = 1;
+    }
+    cin >> M;
+    S = vector<string>(M);
+    cin >> S;
 }
 
 int main() {
     cin.tie(0);
     ios::sync_with_stdio(false);
-    //cout << fixed << setprecision(15);
+    input();
     solve();
     return 0;
 }
